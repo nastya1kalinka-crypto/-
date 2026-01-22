69159738f8592a0dc2e41cb4cc6fa1a11d3de993
nastya1kalinka <nastya1kalinka@gmail.com>
2026-01-23 05:52:20 +1000

    Загруженые файлы seput redash

---
+++ redash-config/redash_make_default.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+# Trivial script to add Redash to the user login profile
+
+cat <<EOF >>~/.profile
+
+# Added by Redash 'redash_make_default.sh' script
+export COMPOSE_PROJECT_NAME=redash
+export COMPOSE_FILE=/opt/redash/compose.yaml
+EOF
+echo "Redash has now been set as the default Docker Compose project"
